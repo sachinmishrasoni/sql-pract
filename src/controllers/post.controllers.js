26c6ba95c1d2329f633c9676f07bbabde5118c4e
SAM <sachinkumarf103@gmail.com>
2025-11-02 20:57:46 +0530

    work on post api

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -11,6 +11,18 @@ import pool from "../config/database.js";
 //     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 // );
 
+// Table Creation Query
+// await pool.query(`
+//     CREATE TABLE IF NOT EXISTS posts (
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         user_id INT NOT NULL,
+//         title VARCHAR(255) NOT NULL,
+//         content TEXT,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+//     )
+// `);
+
 
 // INSERT INTO posts (user_id, title, content) VALUES
 // (1, 'My first post', 'This is my first post content'),
@@ -20,6 +32,7 @@ import pool from "../config/database.js";
 // CREATE POST
 export const createPost = async (req, res) => {
     try {
+        // const { userId } = req.query;
         const { user_id, title, content } = req.body;
 
         if (!user_id || !title || !content) {
@@ -81,7 +94,133 @@ export const getPosts = async (req, res) => {
             results: rows.length,
             data: rows
         });
-        
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong while getting the posts",
+            error
+        })
+    }
+};
+
+// GET POST BY ID
+export const getPostById = async (req, res) => {
+    try {
+        const { postId } = req.params;
+        console.log(postId, req.params);
+
+        const [rows] = await pool.query("SELECT * FROM posts WHERE id = ?", [postId]);
+
+        if (rows.length <= 0) {
+            return res.status(404).json({
+                status: false,
+                message: "Post not found"
+            });
+        }
+
+        res.status(200).json({
+            status: true,
+            data: rows[0]
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong while getting the post",
+            error
+        })
+    }
+};
+
+// UPDATE POST BY ID
+export const updatePostById = async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const { title, content } = req.body;
+
+        const [rows] = await pool.query("UPDATE posts SET title = ?, content = ? WHERE id = ?", [title, content, postId]);
+
+        if (rows.affectedRows <= 0) {
+            return res.status(404).json({
+                status: false,
+                message: "Post not found"
+            });
+        }
+
+        res.status(200).json({
+            status: true,
+            message: "Post updated successfully"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong while updating the post",
+            error
+        })
+    }
+};
+
+// DELETE POST BY ID
+export const deletePostById = async (req, res) => {
+    try {
+        const { postId } = req.params;
+
+        const [rows] = await pool.query("DELETE FROM posts WHERE id = ?", [postId]);
+
+        if (rows.affectedRows <= 0) {
+            return res.status(404).json({
+                status: false,
+                message: "Post not found"
+            });
+        }
+
+        res.status(200).json({
+            status: true,
+            message: "Post deleted successfully"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong while deleting the post",
+            error
+        })
+    }
+};
+
+// GET POSTS BY USER ID
+export const getPostsByUserId = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const [rows] = await pool.query("SELECT * FROM posts WHERE user_id = ?", [userId]);
+        res.status(200).json({
+            status: true,
+            results: rows.length,
+            data: rows
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong while getting the posts",
+            error
+        })
+    }
+};
+
+// GET POSTS BY USER ID AND POST ID
+export const getPostsByUserIdAndPostId = async (req, res) => {
+    try {
+        const { userId, postId } = req.params;
+        const [rows] = await pool.query("SELECT * FROM posts WHERE user_id = ? AND id = ?", [userId, postId]);
+        res.status(200).json({
+            status: true,
+            results: rows.length,
+            data: rows
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json({
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { createPost, getPosts, } from "../controllers/post.controllers.js";
+import { createPost, deletePostById, getPostById, getPosts, updatePostById, } from "../controllers/post.controllers.js";
 
 const postRoutes = express.Router();
 
 postRoutes.post("/", createPost);
 postRoutes.get("/user/:userId/posts", getPosts);
-// postRoutes.get("/:id", getPostById);
-// postRoutes.put("/:id", updatePost);
-// postRoutes.delete("/:id", deletePost);
+postRoutes.get("/:postId", getPostById);
+postRoutes.put("/:postId", updatePostById);
+postRoutes.delete("/:id", deletePostById);
+
+// User based routes
+// postRoutes.get("/user/:userId/posts", getPostsByUserId);
+// postRoutes.get("/user/:userId/posts/:id", getPostById);
+// postRoutes.put("/user/:userId/posts/:id", updatePost);
+// postRoutes.delete("/user/:userId/posts/:id", deletePost);
 
 export default postRoutes;
